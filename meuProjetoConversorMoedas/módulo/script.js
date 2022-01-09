b330eec0f6fa3a5507df2c7d462d64e3a0501fff
Charlon Lima <charlonlima2016@gmail.com>
2022-01-09 16:00:25 -0300

    Meu projeto Conversor Moedas

---
+++ meuProjetoConversorMoedas/módulo/script.js
@@ -0,0 +1,51 @@
+window.onload = function () {
+    const real = document.querySelector("#real");
+    const moedaEstrangeira = document.querySelector("#moedaEstrangeira");
+    const dolar = document.querySelector("#dolar");
+    const euro = document.querySelector("#euro");
+    const clean = document.querySelector("#clean");
+    
+    dolar.addEventListener('click', async function () {
+        if(real.value != ""){
+            let site = "https://economia.awesomeapi.com.br/json/USD-BRL";
+            let dadosFetch = await fetch(site);
+            let dadosJson = await dadosFetch.json();
+            for (let cotacao of dadosJson) {
+                let valorDolar = cotacao["high"];
+                let resultado = Number(real.value) / Number(valorDolar);
+                moedaEstrangeira.value = resultado.toFixed(2);
+            }
+            moedaEstrangeira.style.backgroundColor = 'blue';
+            moedaEstrangeira.style.transition = '1s';
+            moedaEstrangeira.style.color = 'white';
+        } else{
+            alert("Digite um valor no primeiro formulário para converter!");
+        }
+            
+    });
+
+    euro.addEventListener('click', async function () {
+        if(real.value != ""){
+            let site = "https://economia.awesomeapi.com.br/json/EUR-BRL";
+            let dadosFetch = await fetch(site);
+            let dadosJson = await dadosFetch.json();
+            for (let cotacao of dadosJson) {
+                let valorEuro = cotacao["high"];
+                let resultado = Number(real.value) / Number(valorEuro);
+                moedaEstrangeira.value = resultado.toFixed(2);
+            }
+            moedaEstrangeira.style.backgroundColor = 'green';
+            moedaEstrangeira.style.transition = '1s';
+            moedaEstrangeira.style.color = 'white';
+        } else{
+            alert("Digite um valor no primeiro formulário para converter!");
+        }
+            
+    });
+
+    clean.addEventListener('click', function () {
+        real.value = "";
+        moedaEstrangeira.value = "";
+        moedaEstrangeira.style.backgroundColor = 'white';
+    });
+}
